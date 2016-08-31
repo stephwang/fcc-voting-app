559d56f6fb94ef73025365bc2b659a04502f7947
Steph Wang <sw.steph@gmail.com>
2016-08-31 05:39:56 +0000

    voting tool worksmongod --smallfiles!

--- app/controllers/pollPageController.client.js
+++ app/controllers/pollPageController.client.js
@@ -3,6 +3,7 @@ var apiUrl = appUrl + '/api' + window.location.pathname;
 $(document).ready(function(){
     if (window.localStorage.getItem("vote")){
         $("#message").text("voted successfully for " + window.localStorage.getItem("vote"));
+        $("#message").css("color", "#00BCD4");
         window.localStorage.removeItem("vote");
     }
 });
@@ -20,8 +21,10 @@ $( "#vote" ).submit(function( event ) {
     var vote = $('#dropdown').val();
     if(vote == null){
         $("#message").text("Please select an option.");
+        $("#message").css("color", "red");
     } else if (vote == 'other' && $("#otherfield input").val() == ''){
         $("#message").text("Please enter an option.");
+        $("#message").css("color", "red");
     } else {
       $.get( apiUrl , $("#vote").serialize(), function(data) {
           window.localStorage.setItem("vote", data);
